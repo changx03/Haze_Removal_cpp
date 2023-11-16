41b726f3c2d08f97c426b3ddbe0e2934c4083955
Luke Chang <luke@boxfish.nz>
2023-11-17 12:29:07 +1300

    Add ability to save results

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,37 @@
 #include "hazeremoval.h"
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <iostream>
+#include <string>
+#include <filesystem>
 
-using namespace cv;
-using namespace std;
+int main(int argc, char** args)
+{
+    const char* img_path = args[1];
 
-int main(int argc, char **args) {
-	const char * img_path = args[1];
-	Mat in_img = imread(img_path);
-	Mat out_img(in_img.rows, in_img.cols, CV_8UC3);
-	unsigned char * indata = in_img.data;
-	unsigned char * outdata = out_img.data;
+    std::string file_fullname = std::filesystem::path(img_path).filename().string();
+    size_t dotPosition = file_fullname.find_last_of('.');
+    std::string filename = file_fullname.substr(0, dotPosition);
+    std::cout << "Image filename: " << file_fullname << std::endl;
 
-	CHazeRemoval hr;
-	cout << hr.InitProc(in_img.cols, in_img.rows, in_img.channels()) << endl;
-	cout << hr.Process(indata, outdata, in_img.cols, in_img.rows, in_img.channels()) << endl;
-	imshow("out_img", out_img);
-	waitKey(0);
-	// system("pause");
-	return 0;
+    cv::Mat in_img = cv::imread(img_path);
+    cv::Mat out_img(in_img.rows, in_img.cols, CV_8UC3);
+    unsigned char* indata = in_img.data;
+    unsigned char* outdata = out_img.data;
+
+    CHazeRemoval hr;
+    std::cout << hr.InitProc(in_img.cols, in_img.rows, in_img.channels()) << std::endl;
+    std::cout << hr.Process(indata, outdata, in_img.cols, in_img.rows, in_img.channels()) << std::endl;
+
+    // // Save results
+    std::filesystem::path output_path = std::filesystem::path("outputs") / std::string(filename + ".png");
+    std::cout << "Save result to: " << std::string(output_path) << std::endl;
+    cv::imwrite(output_path, out_img);
+
+    cv::imshow("out_img", out_img);
+    cv::waitKey(0);
+    
+    return 0;
 }
