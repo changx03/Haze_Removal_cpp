e73298aa142362b27ad287cbc912ac7127623a76
Luke Chang <luke@boxfish.nz>
2023-11-17 12:28:48 +1300

    Update OpenCV to 4.5.4

--- src/hazeremoval.h
+++ src/hazeremoval.h
@@ -1,44 +1,56 @@
-#ifndef HAZE_REMOVAL_H
-#define HAZE_REMOVAL_H
+#ifndef HAZEREMOVAL_H
+#define HAZEREMOVAL_H
 
 #include "guidedfilter.h"
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
 #include <vector>
 
-typedef struct _pixel {
-	int i;
-	int j;
-	uchar val;
-	_pixel(int _i, int _j, uchar _val) :i(_i), j(_j), val((uchar)_val) {}
+typedef struct _pixel
+{
+    int i;
+    int j;
+    uchar val;
+
+    _pixel(int _i, int _j, uchar _val)
+        : i(_i),
+          j(_j),
+          val((uchar)_val)
+    {
+    }
 } Pixel;
 
 class CHazeRemoval
 {
-public:
-	CHazeRemoval();
-	~CHazeRemoval();
+  public:
+    CHazeRemoval();
+    ~CHazeRemoval();
 
-public:
-	bool InitProc(int width, int height, int nChannels);
-	bool Process(const unsigned char* indata, unsigned char* outdata, int width, int height, int nChannels);
+  public:
+    bool InitProc(int width, int height, int nChannels);
+    bool Process(const unsigned char* indata, unsigned char* outdata, int width, int height, int nChannels);
 
-private:
-	int rows;
-	int cols;
-	int channels;
-
+  private:
+    int rows;
+    int cols;
+    int channels;
 };
 
-void get_dark_channel(const cv::Mat *p_src, std::vector<Pixel> &tmp_vec, int rows, int cols, int channels, int radius);
+void get_dark_channel(const cv::Mat* p_src, std::vector<Pixel>& tmp_vec, int rows, int cols, int channels, int radius);
 
-void get_air_light(const cv::Mat *p_src, std::vector<Pixel> &tmp_vec, cv::Vec3d *p_Alight, int rows, int cols, int channels);
+void get_air_light(const cv::Mat* p_src, std::vector<Pixel>& tmp_vec, cv::Vec3d* p_Alight, int rows, int cols, int channels);
 
-void get_transmission(const cv::Mat *p_src, cv::Mat *p_tran, cv::Vec3d *p_Alight, int rows, int cols, int channels, int radius, double omega);
+void get_transmission(const cv::Mat* p_src, cv::Mat* p_tran, cv::Vec3d* p_Alight, int rows, int cols, int channels, int radius,
+                      double omega);
 
-void guided_filter(const cv::Mat *p_src, const cv::Mat *p_tran, cv::Mat *p_gtran, int r, double eps);
+void guided_filter(const cv::Mat* p_src, const cv::Mat* p_tran, cv::Mat* p_gtran, int r, double eps);
 
-void recover(const cv::Mat *p_src, const cv::Mat *p_gtran, cv::Mat *p_dst, cv::Vec3d *p_Alight, int rows, int cols, int channels, double t0);
+void recover(const cv::Mat* p_src, const cv::Mat* p_gtran, cv::Mat* p_dst, cv::Vec3d* p_Alight, int rows, int cols, int channels,
+             double t0);
 
-void assign_data(unsigned char *outdata, const cv::Mat *p_dst, int rows, int cols, int channels);
+void assign_data(unsigned char* outdata, const cv::Mat* p_dst, int rows, int cols, int channels);
 
-#endif // !HAZE_REMOVAL_H
+#endif // HAZEREMOVAL_H
